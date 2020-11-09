c223bf759bdc721b03b4531417e8d61b3170e201
Jake <jakegreen2016@gmail.com>
2020-11-09 12:57:05 -0900

    create method for grid display

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -18,14 +18,17 @@ module GridComponents
     @b3 = " "
     @c3 = " "   
   end
-  
-  # variables for grid display
-  puts "   A   B   C "
-  puts "1  #{@a1} | #{@b1} | #{@c1}  "
-  puts "  ---+---+---"
-  puts "2  #{@a2} | #{@b2} | #{@c2}  "
-  puts "  ---+---+---"
-  puts "3  #{@a3} | #{@b3} | #{@c3}  "
+
+  # method for grid display
+  def display_grid
+    # variables for grid display
+    puts "   A   B   C "
+    puts "1  #{@a1} | #{@b1} | #{@c1}  "
+    puts "  ---+---+---"
+    puts "2  #{@a2} | #{@b2} | #{@c2}  "
+    puts "  ---+---+---"
+    puts "3  #{@a3} | #{@b3} | #{@c3}  "
+  end
   
 end
 
