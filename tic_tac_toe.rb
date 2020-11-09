6dc0e44a0e7283c12df6477b62f62a9b18a683f6
Jake <jakegreen2016@gmail.com>
2020-11-09 07:34:09 -0900

    make ruby file with pseudo-code

---
+++ tic_tac_toe.rb
@@ -0,0 +1,17 @@
+#class for running the game
+    #display grid class
+        
+        #player input variables
+
+        #variables for grid
+
+    #player class
+
+    #end game conditions
+    
+    #game sequence class
+
+    
+
+
+
