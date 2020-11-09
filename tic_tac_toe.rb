1fd169b94dc2cac9fe9d3523acd279a10b6d6231
Jake <jakegreen2016@gmail.com>
2020-11-09 07:45:24 -0900

    made display grid

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,9 +2,28 @@
     #display grid class
         
         #player input variables
+        #row 1
+        a1 = " "
+        b1 = " "
+        c1 = " "
+        
+        #row 2
+        a2 = " "
+        b2 = " "
+        c2 = " "
+        
+        #row 3
+        a3 = " "
+        b3 = " "
+        c3 = " "
 
         #variables for grid
-
+        puts "   A   B   C "
+        puts "1  #{a1} | #{b1} | #{c1}  "
+        puts "  ---+---+---"
+        puts "2  #{a2} | #{b2} | #{c2}  "
+        puts "  ---+---+---"
+        puts "3  #{a3} | #{b3} | #{c3}  "
     #player class
 
     #end game conditions
