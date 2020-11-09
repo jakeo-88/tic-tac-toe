d0937b0d282cb840946ec704b5f671a8460e6b3d
Jake <jakegreen2016@gmail.com>
2020-11-09 08:11:41 -0900

    formatted code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,40 +1,33 @@
 #class for running the game
-    #display grid class
+    
+  #display grid class
         
-        #player input variables
-        #row 1
-        a1 = " "
-        b1 = " "
-        c1 = " "
-        
-        #row 2
-        a2 = " "
-        b2 = " "
-        c2 = " "
-        
-        #row 3
-        a3 = " "
-        b3 = " "
-        c3 = " "
-
-        #variables for grid
-        puts "   A   B   C "
-        puts "1  #{a1} | #{b1} | #{c1}  "
-        puts "  ---+---+---"
-        puts "2  #{a2} | #{b2} | #{c2}  "
-        puts "  ---+---+---"
-        puts "3  #{a3} | #{b3} | #{c3}  "
-    #player class
-    class Player
-      def initialize(character)
-        @character = @character
-      end
+    #player input variables
+    #row 1
+    a1 = " "
+    b1 = " "
+    c1 = " "
+    
+    #row 2
+    a2 = " "
+    b2 = " "
+    c2 = " "
+    
+    #row 3
+    a3 = " "
+    b3 = " "
+    c3 = "     
+    #variables for grid
+    puts "   A   B   C "
+    puts "1  #{a1} | #{b1} | #{c1}  "
+    puts "  ---+---+---"
+    puts "2  #{a2} | #{b2} | #{c2}  "
+    puts "  ---+---+---"
+    puts "3  #{a3} | #{b3} | #{c3}  "
+  #player class
+  class Player
+    def initialize(character)
+      @character = @character
     end
-    #end game conditions
-    
-    #game sequence class
-
-    
-
-
-
+  end
+  #end game conditions  
