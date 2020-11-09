41785169ffddc7f247f35588491001ead12b7da0
Jake <jakegreen2016@gmail.com>
2020-11-09 09:54:54 -0900

    removed variables from TicTacToe class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -30,45 +30,7 @@ end
 class TicTacToe    
   
         
-    #player input variables
-    #row 1
-    a1 = " "
-    b1 = " "
-    c1 = " "
-    
-    #row 2
-    a2 = " "
-    b2 = " "
-    c2 = " "
-    
-    #row 3
-    a3 = " "
-    b3 = " "
-    c3 = " "   
 
-    #variables for grid
-    puts "   A   B   C "
-    puts "1  #{a1} | #{b1} | #{c1}  "
-    puts "  ---+---+---"
-    puts "2  #{a2} | #{b2} | #{c2}  "
-    puts "  ---+---+---"
-    puts "3  #{a3} | #{b3} | #{c3}  "
-  #player class
-  class Player
-    def initialize(character)
-      @character = @character
-    end
-  end
-  #end game conditions
-  #display grid class
-  class GameGrid
-    #variables for grid
-    puts "   A   B   C "
-    puts "1  #{a1} | #{b1} | #{c1}  "
-    puts "  ---+---+---"
-    puts "2  #{a2} | #{b2} | #{c2}  "
-    puts "  ---+---+---"
-    puts "3  #{a3} | #{b3} | #{c3}  "
   end
   #run game
   def PlayGame 
