5a06de696241e823f8694325cef4e06b6bd89bfb
Jake <jakegreen2016@gmail.com>
2020-11-09 09:21:03 -0900

    move variables to module

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,34 @@
+#module for variables and grid
+module Grid
+  #player input variables
+  #row 1
+  @a1 = " "
+  @b1 = " "
+  @c1 = " "
+  
+  #row 2
+  @a2 = " "
+  @b2 = " "
+  @c2 = " "
+  
+  #row 3
+  @a3 = " "
+  @b3 = " "
+  @c3 = " "   
+  
+  #variables for grid display
+  puts "   A   B   C "
+  puts "1  #{@a1} | #{@b1} | #{@c1}  "
+  puts "  ---+---+---"
+  puts "2  #{@a2} | #{@b2} | #{@c2}  "
+  puts "  ---+---+---"
+  puts "3  #{@a3} | #{@b3} | #{@c3}  "
+  
+end
+
 #class for running the game
-    
-  #display grid class
+class TicTacToe    
+  
         
     #player input variables
     #row 1
@@ -16,7 +44,8 @@
     #row 3
     a3 = " "
     b3 = " "
-    c3 = "     
+    c3 = " "   
+
     #variables for grid
     puts "   A   B   C "
     puts "1  #{a1} | #{b1} | #{c1}  "
@@ -30,4 +59,24 @@
       @character = @character
     end
   end
-  #end game conditions  
+  #end game conditions
+  #display grid class
+  class GameGrid
+    #variables for grid
+    puts "   A   B   C "
+    puts "1  #{a1} | #{b1} | #{c1}  "
+    puts "  ---+---+---"
+    puts "2  #{a2} | #{b2} | #{c2}  "
+    puts "  ---+---+---"
+    puts "3  #{a3} | #{b3} | #{c3}  "
+  end
+  #run game
+  def PlayGame 
+    #create players
+    player1 = Player.new("X")
+    player2 = Player.new("O")
+    #print grid
+    
+    
+  end
+end
