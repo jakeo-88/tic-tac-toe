b0a55e624827f6d73f1a20e4ef360baaae2bcea1
Jake <jakegreen2016@gmail.com>
2020-11-09 12:55:56 -0900

    create method for setting grid variables

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,23 @@
 # module for variables and grid
 module GridComponents
-  # player input variables
-  # row 1
-  @a1 = " "
-  @b1 = " "
-  @c1 = " "
-  
-  # row 2
-  @a2 = " "
-  @b2 = " "
-  @c2 = " "
-  
-  # row 3
-  @a3 = " "
-  @b3 = " "
-  @c3 = " "   
+  # method to set player variables
+  def column_row(a1, b1, c1, a2, b2, c2, a3, b3, c3)
+    # player input variables
+    # row 1
+    @a1 = " "
+    @b1 = " "
+    @c1 = " "
+    
+    # row 2
+    @a2 = " "
+    @b2 = " "
+    @c2 = " "
+    
+    # row 3
+    @a3 = " "
+    @b3 = " "
+    @c3 = " "   
+  end
   
   # variables for grid display
   puts "   A   B   C "
